6d8137d5b16e17dc82b13caf3034f9dbb8280b4e
x-yzt <xyzt@psychonaut.fr>
2020-03-30 21:39:31 +0200

    add dummy drug selector

--- drugcombinator/views.py
+++ drugcombinator/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.urls import reverse
-from drugcombinator.models import Drug
+from drugcombinator.models import Drug, Category
 
 
 def main(request):
 
     drugs = Drug.objects.all()
+    categories = Category.objects.all()
     return render(request, 'drugcombinator/main.html', locals())
