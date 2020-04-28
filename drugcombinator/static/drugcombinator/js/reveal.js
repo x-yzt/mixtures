07c371dda1839a72faa6353911ccb166a316adbf
x-yzt <xyzt@psychonaut.fr>
2020-04-28 23:54:50 +0200

    hide modals when opacity=0

--- drugcombinator/static/drugcombinator/js/reveal.js
+++ drugcombinator/static/drugcombinator/js/reveal.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', event => {
     document.querySelectorAll('.draft-modal-reveal').forEach(elem => {
         elem.addEventListener('click', event => {
             elem.parentElement.style.opacity = 0;
+            elem.parentElement.style.visibility = 'hidden';
             elem.parentElement.parentElement.classList.remove('hidden');
         });
     });
