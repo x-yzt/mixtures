c69080aec8bf6298da2d900ecb7dfe10d65cfa70
x-yzt <xyzt@psychonaut.fr>
2020-06-12 00:08:29 +0200

    add robots.txt

--- mixtures/urls.py
+++ mixtures/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 
-template = lambda name: TemplateView.as_view(template_name=name)
+def template(name, *args, **kwargs):
+    return TemplateView.as_view(template_name=name, *args, **kwargs)
 
 urlpatterns = [
     path('', include('drugcombinator.urls')),
     path('a-propos/', template('mixtures/about.html'), name='about'),
+    path('robots.txt', template('mixtures/robots.txt', content_type='text/plain')),
     path('admin/', admin.site.urls)
 ]
