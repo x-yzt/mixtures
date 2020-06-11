8004dd88b6ea4111ad354f997b6bc5900541c736
x-yzt <xyzt@psychonaut.fr>
2020-06-11 20:46:33 +0200

    add humanlist tag

--- drugcombinator/templatetags/collections.py
+++ drugcombinator/templatetags/collections.py
@@ -19,4 +19,10 @@ def attr(obj, attr_name):
 
 @register.filter
 def cat(obj_1, obj_2):
-    return str(obj_1) + str (obj_2)
+    return str(obj_1) + str(obj_2)
+
+
+@register.filter
+def humanlist(seq):
+    seq = [str(x) for x in seq]
+    return ', '.join(seq[:-1]) + ' et ' + seq[-1]
