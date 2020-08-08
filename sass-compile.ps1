de7aa49a094a151873fccbaaa8ab5fa68c8b96af
x-yzt <xyzt@psychonaut.fr>
2020-08-08 18:15:50 +0200

    switch drugcombinator styles to sass

--- sass-compile.ps1
+++ sass-compile.ps1
@@ -1,7 +1,10 @@
 sass `
+  --watch `
   --no-source-map `
   --style compressed `
+  --load-path ./static/mixtures/sass `
   ./static/mixtures/sass/vendor/materialize.scss:./static/mixtures/css/materialize.min.css  `
   ./static/mixtures/sass/base.scss:./static/mixtures/css/base.min.css `
   ./static/mixtures/sass/admin/base.scss:./static/mixtures/css/admin/base.min.css `
-  ./static/mixtures/sass/admin/forms.scss:./static/mixtures/css/admin/forms.min.css
+  ./static/mixtures/sass/admin/forms.scss:./static/mixtures/css/admin/forms.min.css `
+  ./drugcombinator/static/drugcombinator/sass/combinator.scss:./drugcombinator/static/drugcombinator/css/combinator.min.css
