9165a712d6fb7af15b499d6eb24490cefc62984a
x-yzt <xyzt@psychonaut.fr>
2020-08-08 15:35:18 +0200

    switch base stylesheet to sass

--- sass-compile.ps1
+++ sass-compile.ps1
@@ -2,5 +2,6 @@ sass `
   --no-source-map `
   --style compressed `
   ./static/mixtures/sass/vendor/materialize.scss:./static/mixtures/css/materialize.min.css  `
+  ./static/mixtures/sass/base.scss:./static/mixtures/css/base.min.css `
   ./static/mixtures/sass/admin/base.scss:./static/mixtures/css/admin/base.min.css `
   ./static/mixtures/sass/admin/forms.scss:./static/mixtures/css/admin/forms.min.css
