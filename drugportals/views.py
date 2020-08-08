c1c8290dee18786285c363d90be013cc7bbfe501
x-yzt <xyzt@psychonaut.fr>
2020-08-08 23:35:06 +0200

    add portal view

--- drugportals/views.py
+++ drugportals/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
+from drugportals.models import Portal
+
 
 def portal(request, drug):
-    pass
+
+    try:
+        portal = Portal.objects.get(drug__slug=drug)
+    except Portal.DoesNotExist:
+        raise Http404("Ce portail n'existe pas.")
+    
+    return render(request, 'drugportals/portals.html', locals())
