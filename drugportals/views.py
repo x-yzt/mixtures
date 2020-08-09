4d793926604b5db79353ca815f2432f33ffa6238
x-yzt <xyzt@psychonaut.fr>
2020-08-09 17:49:03 +0200

    add draft portal view + template

--- drugportals/views.py
+++ drugportals/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404
 from drugportals.models import Portal
+from drugcombinator.utils import count_queries
 
-
+@count_queries
 def portal(request, drug):
 
     try:
         portal = Portal.objects.get(drug__slug=drug)
     except Portal.DoesNotExist:
         raise Http404("Ce portail n'existe pas.")
-    
-    return render(request, 'drugportals/portals.html', locals())
+
+    return render(request, 'drugportals/portal.html', locals())
--- sass-compile.ps1
+++ sass-compile.ps1
@@ -7,4 +7,5 @@ sass `
   ./static/mixtures/sass/base.scss:./static/mixtures/css/base.min.css `
   ./static/mixtures/sass/admin/base.scss:./static/mixtures/css/admin/base.min.css `
   ./static/mixtures/sass/admin/forms.scss:./static/mixtures/css/admin/forms.min.css `
-  ./drugcombinator/static/drugcombinator/sass/combinator.scss:./drugcombinator/static/drugcombinator/css/combinator.min.css
+  ./drugcombinator/static/drugcombinator/sass/combinator.scss:./drugcombinator/static/drugcombinator/css/combinator.min.css `
+  ./drugportals/static/drugportals/sass/portals.scss:./drugportals/static/drugportals/css/portals.min.css
