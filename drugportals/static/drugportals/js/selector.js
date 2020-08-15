81e70ebf194fedbc8103285514b72a0f2c910293
x-yzt <xyzt@psychonaut.fr>
2020-08-15 16:53:07 +0200

    add dynamic url updating in portal selector

--- drugportals/static/drugportals/js/selector.js
+++ drugportals/static/drugportals/js/selector.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', event => {
+    const hashDrugSlug = window.location.hash.replace('#', '');
+    const hashDrugBtn = document.getElementById('drug-' + hashDrugSlug);
+    const hashInterCard = document.getElementById('card-' + hashDrugSlug);
+
     const drugsBtns = document.querySelectorAll('.drug-selector .btn');
     const interCards = document.querySelectorAll('.interaction-content');
 
+    function reset_selector() {
+        drugsBtns.forEach(drugBtn => {
+            drugBtn.classList.remove('selected');
+        });
+        interCards.forEach(interCard => {
+            interCard.style.display = 'none';
+        })
+    }
+
     drugsBtns.forEach(drugBtn => {
         const drugSlug = drugBtn.id.replace('drug-', '');
+        const interCard = document.getElementById('card-' + drugSlug);
 
         drugBtn.addEventListener('click', event => {
-            drugsBtns.forEach(drugBtn => {
-                drugBtn.classList.remove('selected');
-            });
+            reset_selector();            
             drugBtn.classList.add('selected');
-            
-            const interCard = document.getElementById('card-' + drugSlug);
-            interCards.forEach(interCard => {
-                interCard.style.display = 'none';
-            })
             interCard.style.display = 'initial';
+
+            window.history.pushState({}, null, '#' + drugSlug);
         });
     });
 
-    interCards.forEach(interCard => {
-        interCard.style.display = 'none';
-    })
+    reset_selector();
+    if (hashDrugBtn) {
+        hashDrugBtn.classList.add('selected');
+        hashInterCard.style.display = 'initial';
+    } else {
+        // Strips invalid anchor tag
+        window.history.replaceState({}, null, window.location.pathname);
+    }
 });
