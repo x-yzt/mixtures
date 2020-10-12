346ebf779622e95052a7e1d6f64288077c841bf7
x-yzt <xyzt@psychonaut.fr>
2020-10-12 16:07:52 +0300

    use css instead of plain height property

--- drugcombinator/static/drugcombinator/js/recap.js
+++ drugcombinator/static/drugcombinator/js/recap.js
@@ -3,6 +3,6 @@ const origin = "https://www.mixtures.info";
 window.addEventListener('message', event => {
     if (event.origin === origin) {
         elem = document.querySelector(`iframe[src="${event.data.url}"]`);
-        elem.height = parseInt(event.data.height) + 30 + 'px';
+        elem.style.height = parseInt(event.data.height) + 30 + 'px';
     }
 });
