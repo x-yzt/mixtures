5bcf7c0682c898dcc4b12686f3844d0bb701671d
x-yzt <xyzt@psychonaut.fr>
2021-07-20 00:04:59 +0200

    configure js i18n catalog

--- mixtures/urls.py
+++ mixtures/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.views.i18n import JavaScriptCatalog
 from django.contrib.sitemaps.views import sitemap as sitemap_view
 
 from mixtures.sitemaps import SITEMAPS
@@ -14,6 +15,7 @@ def template(name, *args, **kwargs):
 urlpatterns = i18n_patterns(
     path('', include('drugcombinator.urls')),
     path('admin/', admin.site.urls),
+    path('i18n.js', JavaScriptCatalog.as_view(), name='i18n'),
 )
 urlpatterns += (
     path('setlang', set_language_view, name='set_language'),
