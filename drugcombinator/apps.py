6b3cc6c7935f6c752c2eaab7d289f01cdde3a199
x-yzt <xyzt@psychonaut.fr>
2022-08-15 19:07:42 +0200

    update django and 3rd-party apps to dj4.1

--- drugcombinator/apps.py
+++ drugcombinator/apps.py
@@ -6,6 +6,7 @@ class DrugcombinatorConfig(AppConfig):
     
     name = 'drugcombinator'
     verbose_name = _("Drug combinator")
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         import drugcombinator.signals
--- drugportals/apps.py
+++ drugportals/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class DrugportalsConfig(AppConfig):
     name = 'drugportals'
     verbose_name = _("Drug portals")
+    default_auto_field = 'django.db.models.AutoField'
