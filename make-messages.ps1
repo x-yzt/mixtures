4fae726b23f8121067661203e5e51389260cdd19
x-yzt <xyzt@psychonaut.fr>
2022-10-04 23:14:18 +0200

    move dev helpers scripts

--- make-messages.ps1
+++
@@ -1,2 +0,0 @@
-python ./manage.py makemessages --locale=fr_FR --ignore=venv/* --ignore=requirements.txt
-python ./manage.py makemessages --domain=djangojs --locale=fr_FR --ignore=venv/* --ignore=requirements.txt
